a150549d57341b8c7051270d982dfc09286c21f6
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2024-01-21 16:56:33 +0000

    define possible tokentypes as  consts

--- token/token.go
+++ token/token.go
@@ -1,10 +1,43 @@
-// define Token struct and our TokenType type
+// define tokens that the lexer will output
+// define Token data struct
+// need type attribute to distinguish b/w diff types of tokens
+// need field to hold literal value of token so we can reuse it
 
 package Token
 
+// defined this as a string so we can use many diff values as TokenTypes and means we can distinguish between different ypes of tokens
+// might not be most performant but for this example, it will suffice
 type TokenType string
 
 type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	// unknown token
+	ILLEGAL = "ILLEGAL"
+	// end of file
+	EOF = "EOF"
+
+	// identifiers and literals
+	IDENT = "IDENT" // add, foobar, x, y etc
+	INT   = "INT"   // 123123
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
