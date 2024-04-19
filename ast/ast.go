c994922d5aae273f18c7a921ea60b05e725fcfd9
s-palmer <zigpalmer@gmail.com>
2024-04-19 21:36:56 +0100

    tests passing

--- ast/ast.go
+++ ast/ast.go
@@ -26,16 +26,22 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
 type LetStatement struct {
 	Token token.Token // the token.LET token
 	Name *Identifier
 	Value Expression
 }
+
 func (ls *LetStatement) statementNode() {}
+
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
 type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
 }
+
 func (i* Identifier) expressionNode() {}
+
 func (i* Identifier) TokenLiteral() string { return i.Token.Literal }
