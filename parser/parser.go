b36b2030c9171acda11f2285d614e344d893f09a
s-palmer <zigpalmer@gmail.com>
2024-12-07 21:58:20 +0000

    set up test for bools and register prefix and func

--- parser/parser.go
+++ parser/parser.go
@@ -57,6 +57,8 @@ func New(l *lexer.Lexer) *Parser {
   p.registerPrefix(token.INT, p.parseIntegerLiteral)
   p.registerPrefix(token.BANG, p.parsePrefixExpression)
   p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+  p.registerPrefix(token.TRUE, p.parseBoolean)
+  p.registerPrefix(token.FALSE, p.parseBoolean)
 
   p.infixParseFns = make(map[token.TokenType]infixParseFn)
   p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -173,6 +175,9 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
   return expression
 }
 
+func (p *Parser) parseBoolean()  ast.Expression {
+  return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
 
 func (p *Parser) noPrefixParseFnError(t token.TokenType) {
   msg := fmt.Sprintf("no prefix parse function for %s found", t)
